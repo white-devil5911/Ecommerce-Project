438675cbb2d1b28fc96bcb69afa4f604193aad82
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-01 22:52:31 +0500

    Move checkout page into React

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/App.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
