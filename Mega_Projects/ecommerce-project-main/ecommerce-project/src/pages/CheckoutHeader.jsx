8cdd0d8a36751a608f2d6e21c218b86e6564c40c
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-02 21:54:32 +0500

    replaced a with link

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/CheckoutHeader.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/CheckoutHeader.jsx
@@ -4,10 +4,10 @@ export function CheckoutHeader(){
     <div className="checkout-header">
     <div className="header-content">
         <div className="checkout-header-left-section">
-    <a href="/">
+    <Link to="/">
       <img className="logo" src="images/logo.png" />
       <img className="mobile-logo" src="images/mobile-logo.png" />
-    </a>
+    </Link>
   </div>
 
   <div className="checkout-header-middle-section">
