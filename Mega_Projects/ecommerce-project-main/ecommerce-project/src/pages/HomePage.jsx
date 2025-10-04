936f4452e69e777777b51483941e19f8ff09810c
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-04 11:59:47 +0500

    use data from backend to generate html

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import axios from 'axios'
+import { useEffect, useState} from 'react';
 import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
-import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
-  axios.get('http://localhost:3000/api/products')
-    .then((response) => {
-      console.log(response.data)
-    });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+        .then((response) => {
+          setProducts(response.data)
+        });
+  }, []);
+
 
     return (
     <>
