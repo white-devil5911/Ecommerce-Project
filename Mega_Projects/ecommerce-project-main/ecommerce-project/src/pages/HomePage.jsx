c5c530af3d725a5b75059f233763cd6b559cc460
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-04 11:38:48 +0500

    Fetch data from backend

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      response.json().then((data) => {
+          console.log(data)
+      });
+    })
+
     return (
     <>
         <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
