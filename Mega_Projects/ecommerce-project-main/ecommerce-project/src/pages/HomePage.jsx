ff7312c1c0b15456e353db4126b1016a4d319778
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-04 11:43:37 +0500

    use axios to get the data

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
+import axios from 'axios'
 import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      response.json().then((data) => {
-          console.log(data)
-      });
-    })
+      console.log(response.data)
+    });
 
     return (
     <>
