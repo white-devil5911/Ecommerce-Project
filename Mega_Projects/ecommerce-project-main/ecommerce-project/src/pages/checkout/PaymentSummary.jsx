bc5b6d45e818b0205d070b277557b9d947d914b5
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-05 23:13:29 +0500

    create order button

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -1,5 +1,17 @@
 import { formatMoney } from "../../utils/money";
-export function PaymentSummary({paymentSummary}) {
+import axios from 'axios';
+import { useNavigate } from "react-router";
+
+
+export function PaymentSummary({paymentSummary, loadCart}) {
+    const navigate = useNavigate();
+
+    const createOrder = async () => {
+      await axios.post('/api/orders')
+      await loadCart()
+      navigate('/orders')
+    };
+
     return (
         
         <div className="payment-summary">
@@ -44,7 +56,8 @@ export function PaymentSummary({paymentSummary}) {
                             </div>
                       </div>
           
-                      <button className="place-order-button button-primary">
+                      <button className="place-order-button button-primary"
+                        onClick={createOrder}>
                         Place your order
                       </button>
           </>
