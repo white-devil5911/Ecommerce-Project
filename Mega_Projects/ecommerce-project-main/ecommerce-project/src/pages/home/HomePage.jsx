8a6953a75a6f0429023066001b07fa2a61a9aecd
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-05 22:29:55 +0500

    create add to cart feature

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/HomePage.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import CheckmarkIcon from '../../assets/images/icons/checkmark.png';
 import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid'
 
-export function HomePage({ cart }) {
+export function HomePage({ cart , loadCart}) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export function HomePage({ cart }) {
       <title>Ecommerce Project</title>
   
         <div className="home-page">
-          <ProductsGrid products={products}/>
+          <ProductsGrid products={products} loadCart={loadCart} />
         </div>
     </>
     );
