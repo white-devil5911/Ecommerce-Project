7a105486b00ae1ae017efa7e2ecafcf857491969
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-06 21:14:43 +0500

    test clicking add to cart button

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/Product.test.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { it, expect, describe, vi } from 'vitest';
 import { Product } from './Product';
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+
+vi.mock('axios');
 
 describe('Product Component', () => {
     it('displays the product details correctly', () => {
@@ -38,5 +42,35 @@ describe('Product Component', () => {
         expect(
             screen.getByText('87')
         ).toBeInTheDocument();
+    });
+
+    it('adds a product to the cart', async() => {
+        const product =   {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+              stars: 4.5,
+              count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        }
+
+        const loadCart = vi.fn();
+        render(<Product product={product} loadCart={loadCart}/>);
+
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        );  
+        expect(loadCart).toHaveBeenCalled()
     });
 });
