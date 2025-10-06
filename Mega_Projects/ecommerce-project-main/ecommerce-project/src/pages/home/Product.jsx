17abec8a4e7e63394bdb83c21a524dd5d0100d90
white-devil5911 <muhammadqasim5911@gmail.com>
2025-10-06 21:01:49 +0500

    create integration test for product component

--- Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/Product.jsx
+++ Mega_Projects/ecommerce-project-main/ecommerce-project/src/pages/home/Product.jsx
@@ -23,6 +23,7 @@ export function Product({product, loadCart}) {
         <div className="product-container">
         <div className="product-image-container">
           <img className="product-image"
+            data-testid="product-image"
             src={product.image} />
         </div>
 
@@ -32,6 +33,7 @@ export function Product({product, loadCart}) {
 
         <div className="product-rating-container">
           <img className="product-rating-stars"
+            data-testid="product-rating-stars-image"
             src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
           <div className="product-rating-count link-primary">
             {product.rating.count}
